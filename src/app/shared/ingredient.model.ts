e4152b58acd0412fb32af8c4f8bc72aaba90fa64
Ron Hemmers <rhemmers@hotmail.com>
2023-02-12 21:28:52 -0500

    Updates shopping list component

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,12 @@
+export class Ingredient {
+    // public name: string;
+    // public amount: number;
+
+//     constructor(name: string, amount: number) {
+//         this.name = name;
+//         this.number = number;
+//     }
+
+    // shorter version of the above
+    constructor(public name: string, public amount: number) {}
+}
