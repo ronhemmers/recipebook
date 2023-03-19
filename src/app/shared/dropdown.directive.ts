f273e95959575503c69c03e6638508723b4a48ab
Ron Hemmers <rhemmers@hotmail.com>
2023-03-19 19:37:42 -0400

    Modify the DropDownDirective hostListener so that you can close the dropdown (recipe-detail, header) from anywhere

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,14 +1,16 @@
-import { Directive, HostListener, HostBinding } from '@angular/core';
+import { Directive, HostListener, HostBinding, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]'
 })
 export class DropdownDirective {
 
-  // Attach/detach a css class to the Manage Recipe button
+  /**
+   * Attach/detach the CSS 'open' class to the buttons we want to work with:
+   * 1. Manage Recipe button (recipe-detail component)
+   * 2. Manage button (header component)
+   */
   @HostBinding('class.open') isOpen: boolean = false;
-
-  constructor() { }
 
   /**
    * Toggle the isOpen property when the event 'click' is detected
@@ -16,9 +18,19 @@ export class DropdownDirective {
    * 1. recipe-detail.component.html
    * 2. header.component.html
    */
-  @HostListener('click') toggleOpen() {
+  // @HostListener('click') toggleOpen() {
+  //   console.log('DropdownDirective::toggleOpen()');
+  //   this.isOpen = !this.isOpen;
+  // }
+  /**
+   * Place the listener on the document instead of the dropdown.
+   * Now we can close the dropdown by clicking anywhere.
+   */
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
     console.log('DropdownDirective::toggleOpen()');
-    this.isOpen = !this.isOpen;
+    this.isOpen = this.elementRef.nativeElement.contains(event.target) ? !this.isOpen : false;
   }
+
+  constructor(private elementRef: ElementRef) {}
 
 }
